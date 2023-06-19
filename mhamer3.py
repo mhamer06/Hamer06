fb0459a78c5b7ff75536079e42d6b90f932906be
mhamer06 <48219174+mhamer06@users.noreply.github.com>
2023-06-19 18:04:35 -0400

    Create mhamer3.py

---
+++ mhamer3.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+
+A=[1,2,3.15, 'edureka!']
+
+A[1] = 'Changed 2nd element'
+
+print(A)
+[s350556@jumpadm1 Hamer06]$ cat num.py
+#!/usr/bin/python
+
+A=10
+B=10.65
+C=10+6j
+D = 5 + 4j
+
+print(D-C)
+
+print(A,B,C)
