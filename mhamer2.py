9d3465f5cf0856f6ac23382abb3aa6c5f827ce9b
mhamer06 <48219174+mhamer06@users.noreply.github.com>
2023-06-19 18:04:04 -0400

    Create mhamer2.py

---
+++ mhamer2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+
+print("Hello World")
+print("Hello World")
+print("")
+print("")
+
+
+print("Welcome to Edureka")
+
+print("Happy Learning \n\n\n\n Welcome to Python")
+
+
+[s350556@jumpadm1 Hamer06]$ cat list.py
+#!/usr/bin/python
+
+
+A=[1,2,3.15, 'edureka!']
+
+A[1] = 'Changed 2nd element'
+
+print(A)
